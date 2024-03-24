785ee9fa6b4b7e2bee6ac42197c2f5c456955091
HONG SE HYUN <sese0733@naver.com>
2024-03-24 22:53:31 +0900

    feat: 로딩바 추가 및 버튼 클릭 시 색상 변경 효과 추가

--- src/App.js
+++ src/App.js
@@ -1,39 +1,81 @@
 import './App.css';
 import { useEffect, useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import WeatherBox from './component/WeatherBox';
 import WeatherButton from './component/WeatherButton';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import ClipLoader from "react-spinners/ClipLoader";
 
 function App() {
-
   const [weather, setWeather] = useState(null);
+  const [city, setCity] = useState('');
+  const [loading, setLoading] = useState(true);
+  const [selectedCity, setSelectedCity] = useState('');
+
+  const cities = ['seoul', 'paris', 'new york', 'tokyo'];
+
   const getCurrentLocation = () => {
     navigator.geolocation.getCurrentPosition((position) => {
-      let lat = position.coords.latitude
-      let lon = position.coords.longitude
+      let lat = position.coords.latitude;
+      let lon = position.coords.longitude;
 
-      getWeatherByCurrentLocation(lat, lon)
+      getWeatherByCurrentLocation(lat, lon);
+      setSelectedCity('');
     });
   }
 
-  const getWeatherByCurrentLocation = async(lat, lon) => {
-    let apiKey = process.env.REACT_APP_WEATHER_APP_API_KEY;
+  const apiKey = process.env.REACT_APP_WEATHER_APP_API_KEY;
+
+  const getWeatherByCurrentLocation = async (lat, lon) => {
     let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`
+    setLoading(true);
     let response = await fetch(url);
     let data = await response.json();
     console.log(data);
     setWeather(data);
+    setLoading(false);
   }
 
+  const getWeatherByCity = async () => {
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    setLoading(true);
+    let response = await fetch(url);
+    let data = await response.json();
+    console.log(data);
+    setWeather(data);
+    setLoading(false);
+  }
+
+  const handleSetCity = (city) => {
+    setCity(city);
+    setSelectedCity(city);
+  };
+
   useEffect(() => {
-    getCurrentLocation();
-  }, [])
+    if (city === "") {
+      getCurrentLocation();
+    } else {
+      getWeatherByCity();
+    }
+  }, [city])
+
   return (
     <div>
-      <div className='container'>
-        <WeatherBox weather={weather}/>
-        <WeatherButton/>
-      </div>
+      {loading ? (
+        <div className='container'>
+          <ClipLoader
+            color="#f88cb6"
+            loading={loading}
+            size={150}
+            aria-label="Loading Spinner"
+            data-testid="loader"
+          />
+        </div>
+      ) : (
+        <div className='container'>
+          <WeatherBox weather={weather} />
+          <WeatherButton cities={cities} setCity={handleSetCity} getCurrentLocation={getCurrentLocation} selectedCity={selectedCity}/>
+        </div>
+      )}
     </div>
   );
 }
--- src/component/WeatherButton.js
+++ src/component/WeatherButton.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import { Button } from 'react-bootstrap';
 
-const WeatherButton = () => {
+const WeatherButton = ({ cities, setCity, getCurrentLocation, selectedCity }) => {
+
   return (
     <div>
-        <Button variant="light">current location</Button>
+        <Button
+        variant={selectedCity === '' ? 'secondary' : 'light'} onClick={() => getCurrentLocation()}
+        >current location</Button>
+        {cities.map((item, index)=> (
+          <Button variant={selectedCity === item ? "secondary" : "light"} key={index} onClick={()=>setCity(item)}>{item}</Button>
+        ))}
     </div>
   )
 }
